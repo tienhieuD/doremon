c42baf676e309db6c494be4973c7dfa2ba3f5ba0
Vinh Ngo <nanyangbk@gmail.com>
2022-02-18 00:09:36 +0700

    Test build

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <>
       <div className="w-screen h-screen flex flex-col items-center justify-center">
-        <h1 className="">Hello world</h1>
+        <h1 className="">Hello world !!!</h1>
         <h1 className="">Let edit me now!</h1>
       </div>
     </>
