948c9f0de97b7905f792e5c05f25f98500e0545f
Bui Thi Hong Nhung <buihongnhung0208@gmail.com>
2022-02-20 12:25:39 +0700

    fix padding

--- components/core-team-member/index.js
+++ components/core-team-member/index.js
@@ -68,23 +68,22 @@ const data = [
 export default function CoreTeamMember() {
   const renderList = () => {
     return data.map((el, index) => (
-      <div
-        key={index}
-        className="sm:w-64 w-80 h-auto bg-bgCoreTeam border rounded-2xl border-gray-200 px-4 py-6 text-center my-1 mx-auto"
-      >
-        <img src={el.image} className="w-32 h-32 mb-4 m-auto" />
-        <div className="text-xl leading-7 font-semibold not-italic mb-2 font-inter text-bgFqa">
-          {el.name}
+      <div className="my-4 px-4 sm:w-1/2 lg:w-1/4 w-1" key={index}>
+        <div className="sm:w-full w-80 h-auto bg-bgCoreTeam border rounded-2xl border-gray-200 px-4 py-6 text-center mx-auto ">
+          <img src={el.image} className="w-32 h-32 mb-4 m-auto" />
+          <div className="text-xl leading-7 font-semibold not-italic mb-2 font-inter text-bgFqa">
+            {el.name}
+          </div>
+          <div className="text-sm leading-4 font-semibold not-italic mb-2 text-red-500 font-inter">
+            {el.title}
+          </div>
+          <div className="text-sm leading-4 font-normal not-italic mb-6 text-gray-400 font-inter">
+            {el.description}
+          </div>
+          <a href={el.linkedin} target="_blank">
+            <img src="/linkedin.svg" className="w-6 h-6 m-auto" />
+          </a>
         </div>
-        <div className="text-sm leading-4 font-semibold not-italic mb-2 text-red-500 font-inter">
-          {el.title}
-        </div>
-        <div className="text-sm leading-4 font-normal not-italic mb-6 text-gray-400 font-inter">
-          {el.description}
-        </div>
-        <a href={el.linkedin} target="_blank">
-          <img src="/linkedin.svg" className="w-6 h-6 m-auto" />
-        </a>
       </div>
     ))
   }
@@ -94,9 +93,7 @@ export default function CoreTeamMember() {
         <div className="sm:text-5xl text-3xl leading-10 font-black not-italic sm:mb-16 m-10 text-bgFqa font-cabinet text-center">
           Core team member
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 ">
-          {renderList()}
-        </div>
+        <div className="sm:flex sm:flex-wrap sm:px-20">{renderList()}</div>
       </div>
     </div>
   )
