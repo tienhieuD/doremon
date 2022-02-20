b9d8cb9068ab5a67791203de36d32672be2422e0
Bui Thi Hong Nhung <buihongnhung0208@gmail.com>
2022-02-20 09:41:43 +0700

    add key for map

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
 import { useEffect, useState } from 'react'
-import Faq from '../components/faq'
-import CoreTeamMember from '../components/core-team-member'
-import InvestorsPartners from '../components/investors-partners'
 
 export default function HomePage() {
   return (
     <>
       <div className="w-screen h-screen flex flex-col items-center justify-center">
-        {/* <Faq /> */}
-        {/* <CoreTeamMember/> */}
-        <InvestorsPartners />
+        <h1 className="">Hello world !!!</h1>
+        <h1 className="">Let edit me now!</h1>
       </div>
     </>
   )
